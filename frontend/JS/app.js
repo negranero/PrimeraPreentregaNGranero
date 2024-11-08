34f4bbe3c57cab52e6e715032ddd98d4134474e2
ngranero <nataliagranero@gmail.com>
2024-11-07 22:30:41 -0300

    primera preentrega

---
+++ frontend/JS/app.js
@@ -0,0 +1,12 @@
+console.log('Hola a todos!');
+
+const edadUsuario= prompt("ingrese su edad");
+console.log(edadUsuario);
+debugger
+
+// IF ELSE
+if(edadUsuario >= 18){
+    console.log("podes entrar");
+}else {
+    console.log("no podes entrar");
+}
