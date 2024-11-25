1a687a8a7951eedd5a1c2cfc6a763e806d9aba94
ngranero <nataliagranero@gmail.com>
2024-11-25 20:39:06 -0300

    entrega 2 NataliaGRanero

--- frontend/JS/app.js
+++ frontend/JS/app.js
@@ -1,14 +1,16 @@
-// Constructor para crear productos
-function Producto(nombre, categoria, precio, stock) {
-    if (!nombre || typeof nombre !== "string") throw new Error("El nombre debe ser una cadena válida.");
-    if (!categoria || typeof categoria !== "string") throw new Error("La categoría debe ser una cadena válida.");
-    if (!precio || typeof precio !== "number" || precio <= 0) throw new Error("El precio debe ser un número mayor que 0.");
-    if (!stock || typeof stock !== "number" || stock < 0) throw new Error("El stock debe ser un número no negativo.");
+// Clase Producto
+class Producto {
+    constructor(nombre, categoria, precio, stock) {
+        if (!nombre || typeof nombre !== "string") throw new Error("El nombre debe ser una cadena válida.");
+        if (!categoria || typeof categoria !== "string") throw new Error("La categoría debe ser una cadena válida.");
+        if (!precio || typeof precio !== "number" || precio <= 0) throw new Error("El precio debe ser un número mayor que 0.");
+        if (!stock || typeof stock !== "number" || stock < 0) throw new Error("El stock debe ser un número no negativo.");
 
-    this.nombre = nombre;
-    this.categoria = categoria;
-    this.precio = precio;
-    this.stock = stock;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
 }
 
 // Variables iniciales
@@ -19,7 +21,7 @@ let productos = [
     new Producto("Mesa", "Muebles", 200, 3),
 ];
 
-// Función para agregar un nuevo producto
+// Método para agregar un nuevo producto
 function agregarProducto(nombre, categoria, precio, stock) {
     try {
         const nuevoProducto = new Producto(nombre, categoria, precio, stock);
